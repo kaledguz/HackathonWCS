ffe527024dbb97f9ab3c7bd1543ab125518fd003
kaled guzman <guzman.kaled.pro@gmail.com>
2024-07-03 13:53:34 +0200

    Création de la NavBar avec tailwind

--- Client/src/Components/NavBar.jsx
+++ Client/src/Components/NavBar.jsx
@@ -1,5 +1,24 @@
 function NavBar() {
-  return <div></div>
+  return (
+    <nav className="bg-gray-500 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg font-bold">
+          <a href="/">HackathonWCS</a>
+        </div>
+        <div className="space-x-4">
+          <a href="/" className="text-gray-300 hover:text-white">
+            Home
+          </a>
+          <a href="/about" className="text-gray-300 hover:text-white">
+            Connexion
+          </a>
+          <a href="/contact" className="text-gray-300 hover:text-white">
+            Contact
+          </a>
+        </div>
+      </div>
+    </nav>
+  )
 }
 
 export default NavBar
--- Client/src/Pages/Home.jsx
+++ Client/src/Pages/Home.jsx
@@ -1,7 +1,7 @@
-function Home() {
+const Home = () => {
   return (
     <div>
-      <h1>Ceci est la Home Page</h1>
+      <h1>Home Page</h1>
     </div>
   )
 }
