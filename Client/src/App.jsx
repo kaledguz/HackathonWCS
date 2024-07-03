1f0b6d9ee89a8b03d2b65691326b20d5c693f613
kaled guzman <guzman.kaled.pro@gmail.com>
2024-07-03 11:56:48 +0200

    ajout des fichiers et dossier qu'on aurai besoin

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,13 @@
+import NavBar from "./Components/NavBar"
+import { Outlet } from "react-router-dom"
+
 function App() {
-  return <></>
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  )
 }
 
 export default App
--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import App from "./App.jsx"
+import "./index.css"
+import Home from "./Pages/Home.jsx"
+import Contact from "./Pages/Contact.jsx"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+  },
+])
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
