5653222ab24a51fab64c8985371e87f508d10d47
Dim2960 <dim.lefebvre60@gmail.com>
2024-07-04 10:40:41 +0200

    Ajout du fichier d'importation librairie

---
+++ ML/ML_knn.py
@@ -0,0 +1,5 @@
+from modules.importation import (pd, np, Pipeline, 
+                                StandardScaler, FunctionTransformer, 
+                                MultiLabelBinarizer, ColumnTransformer, 
+                                SimpleImputer, BaseEstimator, 
+                                TransformerMixin, NearestNeighbors)
---
+++ ML/importation.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, FunctionTransformer, MultiLabelBinarizer
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.neighbors import NearestNeighbors
+
