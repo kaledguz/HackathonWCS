380a810f7942e6c9ee47b096a22818c19b0a472f
Dim2960 <dim.lefebvre60@gmail.com>
2024-07-04 11:36:48 +0200

    Ajout fichier extration de la bdd vers .parquet pour exploitation ML

--- ML/ML_knn.py
+++ ML/ML_knn.py
@@ -1,5 +1,54 @@
-from modules.importation import (pd, np, Pipeline, 
+from importation import (pd, np, Pipeline, 
                                 StandardScaler, FunctionTransformer, 
                                 MultiLabelBinarizer, ColumnTransformer, 
                                 SimpleImputer, BaseEstimator, 
                                 TransformerMixin, NearestNeighbors)
+
+
+
+def fetch_data_parquet():
+    pass
+
+
+def model_knn_module(df:pd.DataFrame, id_user:int, preprocessor:Pipeline)->tuple[int]:
+    """
+    Entraîne un modèle KNN pour trouver les mentor les plus proches
+
+    Args:
+        df (pd.DataFrame): Le DataFrame contenant les données des mentor et mentoré.
+        id_user (int): L'id de l'utilisateur recherchant un mentor
+        preprocessor: Le préprocesseur utilisé pour transformer les données.
+
+    Returns:
+        tupples: tupple retournant les 3 id des utilisateurs ayant le statut de mentor
+    """
+
+    # Défintion du model
+    KNNmodel = NearestNeighbors(n_neighbors= 4, algorithm= "auto")
+
+    # Combinaison du preprocessing et du model
+    pipeline_with_knn = Pipeline(steps=[
+        ('preprocessor', preprocessor),
+        ('knn', KNNmodel)
+    ])
+
+    # entarinement du model
+    pipeline_with_knn.fit(df)
+
+    
+    
+    #recuperation des information du user selectionné
+    query_features = df[df['id_user'] == id_user].drop(columns=['id_user'], axis=1)
+
+    # infos transformé sur le film selectionné
+    query_features_processed = pipeline_with_knn.named_steps['preprocessor'].transform(query_features)
+
+    # utilisation du model knn plus proche voisin du film -> recup des indiczes des films
+    id_knn = pipeline_with_knn.named_steps['knn'].kneighbors(query_features_processed, return_distance= False)
+
+    # recuperation des infos par rapprot au id trouvés par le model knn
+    df_knn = df.iloc[id_knn[0]]
+
+
+
+    return df_knn.reset_index()
